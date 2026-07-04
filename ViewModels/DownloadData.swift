98eebe1b234e14a0b8dd7a4695401feb0b0d7589
Shani <106402127+ShaniDan@users.noreply.github.com>
2026-07-04 10:07:31 -0700

    Practicing file download from url

---
+++ ViewModels/DownloadData.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadData.swift
+//  HomeProject
+//
+//  Created by Shakhnoza Mirabzalova on 7/2/26.
+//
+
+import Foundation
+
+
+struct fileDownLoad {
+    
+    func download() async {
+        let downloadTask = URLSession.shared.downloadTask(with: URL) {
+            
+        }
+    }
+}
